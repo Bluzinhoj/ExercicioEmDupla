4b51ec50fdf332f87aa398b08d91f7d3646761e5
Blu <109821453+Bluzinhoj@users.noreply.github.com>
2026-09-15 20:22:04 -0300

    Movi o main para uma classe separada
    O instrutor informou que deveríamos fazer os testes main em uma classe
    separada do arquivo do funcionário.

--- Funcionario.java
+++ Funcionario.java
@@ -78,18 +78,4 @@ public class Funcionario
 
         return salarioDescontado;
     }
-
-    public static void main(String[] args)
-    {
-        System.out.println("teste: criar funcionarios");
-
-        Funcionario funcionario1 = new Funcionario("Robert", "8712871121");
-        Funcionario funcionario2 = new Funcionario("Jose", "19827319827", 1000);
-        Funcionario funcionario3 = new Funcionario("Helena", "9085749587", 1500, 4);
-        Funcionario funcionario4 = new Funcionario("Maria", "908587621587", 1500, 6);
-
-        // TODO: Imprimir informações dos funcionários
-
-    }
-
 }
